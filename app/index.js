c202aa486074a6149029832d906bcef09d4d4ad9
xiaoweimei <1227380089@qq.com>
2019-07-31 20:48:53 +0800

    引入webpack-dev-server

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ function component(){
 	var element=$('<div></div>')
 
 	/* lodash is required for the next line to work */
-	element.html(_.join(['Hello','webpack'],' '))
+	element.html(_.join(['Hi','webpack'],' '))
 
 	return element.get(0);
 }
