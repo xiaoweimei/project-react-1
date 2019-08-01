5a158d704b64b65d3fd3016b28f89334e9940a27
xiaoweimei <1227380089@qq.com>
2019-08-01 11:41:26 +0800

    添加监听函数

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -3,6 +3,13 @@ import React from 'react'
 
 export default class TodoInput extends React.Component{
 	render(){
-		return <input type="text" defaultValue={this.props.content} />
+		return <input type="text" defaultValue={this.props.content} 
+			onKeyPress={this.submit}/>
+	}
+	submit(e){
+		console.log(e)
+		if(e.key==="Enter"){
+			console.log('用户按回车了')
+		}
 	}
 }
